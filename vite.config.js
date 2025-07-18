1ffa4b009f04a991578c95646ae36eb8177142da
Alan <badninjarabbit@gmail.com>
2025-07-19 03:26:05 +0500

    7th commit - Fixed the issue of: Page Refresh and Directly going to subdirectory page, but updating vite.config to Generate 404.html. Correct ver.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/My-cloud-repo/'
+  base: '/My-cloud-repo/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, 'index.html')
+      }
+    }
+  }
 })
